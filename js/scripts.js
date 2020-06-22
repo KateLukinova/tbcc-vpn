5bdce119d26189fe65ded7acfe59e5626a230f3a
Екатерина Лукинова <katyushta@rambler.ru>
2020-06-22 18:57:45 +0300

    plan

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 
 $( document ).ready(function() {
 
+    AOS.init({
+        duration: 1000,
+    })
+
+    $(".pricing-item-click").click(function() {
+        $(".pricing-item-click").removeClass('active')
+        $(this).addClass('active')
+    });
+
+    $(".collapse-button").click(function () {
+        $(this).toggleClass('show')
+        $('.collapse-content').slideToggle()
+
+    });
 });
